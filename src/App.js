280db610bda38e9c77696a7d359fb6b044c64029
Karolis Daugerdas <daugerdas@gmail.com>
2023-03-27 21:34:45 +0300

    add more data

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import "./App.css";
 
 function App() {
   const [flipped, setFlipped] = useState(false);
+  const [index, setIndex] = useState(0);
 
   return (
     <div className="App">
       <h1>Frontend Cards</h1>
-      <Card sideA={data[0].sideA} sideB={data[0].sideB} flipped={flipped}/>
+      <Card sideA={data[index].sideA} sideB={data[index].sideB} flipped={flipped}/>
       <button className="action-button" onClick={() => setFlipped(prev => !prev)}>Flip card</button>
-      <button className="action-button">Next card</button>
+      <button className="action-button" onClick={() => setIndex(Math.floor(Math.random() * data.length))}>Next card</button>
     </div>
   );
 }
