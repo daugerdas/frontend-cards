dd574fd04aeb2020c7b8a4a6a898f7a7eb88610c
Karolis Daugerdas <daugerdas@gmail.com>
2023-03-27 20:10:58 +0300

    add data and fliping feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import Card from "./components/Card";
+import data from "./data/html.json"
 import "./App.css";
 
 function App() {
+  const [flipped, setFlipped] = useState(false);
+
   return (
     <div className="App">
       <h1>Frontend Cards</h1>
-      <Card />
-      <button className="action-button">Flip card</button>
+      <Card sideA={data[0].sideA} sideB={data[0].sideB} flipped={flipped}/>
+      <button className="action-button" onClick={() => setFlipped(prev => !prev)}>Flip card</button>
       <button className="action-button">Next card</button>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
 import './Card.css';
 
-function Card() {
+function Card(props) {
+  const { sideA, sideB, flipped } = props;
+
   return (
     <div className="card">
-      <div className="card-text">Card text</div>
+      <div className="card-text">{flipped ? sideB : sideA}</div>
     </div>
   );
 }
